99b40ec814c28bec47e6c0a8a4ee823d359876f1
chen <23054115+cwkang1998@users.noreply.github.com>
2023-07-22 22:39:26 +0200

    feat: implement selectable grid

--- ui/src/components/GeneratedImageGrid.tsx
+++ ui/src/components/GeneratedImageGrid.tsx
@@ -1,8 +1,49 @@
-export const GeneratedImageGrid = ({ imgUrls }: { imgUrls: string[] }) => {
+import { useState } from "react";
+
+export const GeneratedSelectableImageGrid = ({
+  imgUrls,
+  onAllImageSelected,
+  maxSelectable,
+  onMaxImageSelected,
+}: {
+  imgUrls: string[];
+  onAllImageSelected: (selectedImgs: string[]) => void;
+  maxSelectable: number;
+  onMaxImageSelected?: () => void;
+}) => {
+  const [selected, setSelected] = useState<number[]>([]);
+
+  const selectionInteraction = (idx: number) => () => {
+    if (selected.includes(idx)) {
+      return setSelected((prev) => prev.filter((prevId) => prevId !== idx));
+    }
+
+    if (selected.length === maxSelectable) {
+      return onMaxImageSelected ? onMaxImageSelected() : undefined;
+    }
+
+    // Adding to selection
+    setSelected((prev) => [...prev, idx]);
+    const newSelected = [...selected, idx];
+
+    if (selected.length + 1 === maxSelectable) {
+      return onAllImageSelected(
+        imgUrls.filter((v, urlIdx) => newSelected.includes(urlIdx)),
+      );
+    }
+
+    return undefined;
+  };
+
   return (
     <div className="grid grid-cols-3 md:grid-cols-5 gap-2">
-      {imgUrls.map((url) => (
-        <div>
+      {imgUrls.map((url, idx) => (
+        <div
+          onClick={selectionInteraction(idx)}
+          className={`rounded-lg ${
+            selected.includes(idx) ? "outline outline-2 outline-pink-500" : ""
+          }`}
+        >
           <img
             className="h-auto max-w-full rounded-lg"
             src={url}
--- ui/src/page/Generate.tsx
+++ ui/src/page/Generate.tsx
@@ -1,7 +1,6 @@
-import { useState } from "react";
 import { Layout } from "../Layout";
 import { Container } from "../components/Container";
-import { GeneratedImageGrid } from "../components/GeneratedImageGrid";
+import { GeneratedSelectableImageGrid } from "../components/GeneratedImageGrid";
 
 const imgUrls = [
   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
@@ -19,17 +18,29 @@ const imgUrls = [
   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
+  "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"
 ]
 export const Generate = () => {
   
   return (
     <Layout>
       <Container>
-        <section className="flex flex-col w-full p-4">
-          <h4 className="font-bold text-center md:text-left"></h4>
-          <hr />
-          <div className="py-4">
-            <GeneratedImageGrid imgUrls={imgUrls} />
+        <section className="flex flex-col w-full p-4 text-center">
+          <h4 className="text-center font-bold">Generated art</h4>
+          <p className="text-center text-slate-600">
+            Select 2 images for breeding the next generation of children.
+          </p>
+          <div className="md:px-12 px-4 py-4">
+            <GeneratedSelectableImageGrid imgUrls={imgUrls} onAllImageSelected={(urls) => console.log(urls)} maxSelectable={2}/>
           </div>
         </section>
       </Container>
